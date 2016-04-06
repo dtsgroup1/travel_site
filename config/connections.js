81816996ffc287085c849fccbc79491b98c1f133
ClearlyJosh <jpclark@unomaha.edu>
2016-04-05 20:31:45 -0500

    Openshift Deployment
    ..if there's another commit soon, it means this one might not have been
    a resounding success

--- config/connections.js
+++ config/connections.js
@@ -48,6 +48,15 @@ module.exports.connections = {
     database: 'travel_site'
   },
 
+  openshift_mysql: {
+    adapter: 'sails-mysql',
+    user: 'mashup',
+    password: 'thisismypassword',
+    database: 'group1mashup',
+    host: process.env.OPENSHIFT_MYSQL_DB_HOST,
+    port: process.env.OPENSHIFT_MYSQL_DB_PORT
+  },
+
   /***************************************************************************
   *                                                                          *
   * MongoDB is the leading NoSQL database.                                   *
