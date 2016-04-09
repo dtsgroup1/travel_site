a257f7b4ada82c7ad6d188e061b7d239756aef38
ClearlyJosh <jpclark@unomaha.edu>
2016-04-09 10:35:17 -0500

    working api address

--- api/controllers/FlightController.js
+++ api/controllers/FlightController.js
@@ -22,17 +22,17 @@ module.exports = {
       //appKey: 4cc49ed437eb48a4729722360e30ac41
 
       //parse trip date
-      date_array
+      date_array = trip.departureDate.split("/");
 
       //flight by route, departing on given date
-      //curl -v  -X GET "https://api.flightstats.com/flex/schedules/rest/v1/json/from/OMA/to/LAX/departing/2016/04/06?appId=0efb0de0&appKey=4cc49ed437eb48a4729722360e30ac41"
+      //curl -v  -X GET "https://api.flightstats.com/flex/schedules/rest/v1/json/from/OMA/to/DFW/departing/2016/04/09?appId=0efb0de0&appKey=4cc49ed437eb48a4729722360e30ac41"
 
       function pick_flight(trip, callback) {
        //Need to parse trip.departureDate for our API (/trip.year/trip.month/trip.day)
        options = {
        host: 'api.flightstats.com',
        port: 80,
-       path: '/flex/flightstatus/rest/v2/json/route/status/'+trip.origin+'/'+trip.destination+'/dep/'+trip.year+'/'+trip.month+'/'+trip.day,
+       path: '/flex/flightstatus/rest/v2/json/route/status/'+trip.origin+'/'+trip.destination+'/dep/'+date_array[3]+'/'+date_array[2]+'/'+date_array[1],
        method: 'GET'
        };
 
