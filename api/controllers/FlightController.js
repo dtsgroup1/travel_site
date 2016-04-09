2b25986991953645342a40e6c400af82d7c20163
ClearlyJosh <jpclark@unomaha.edu>
2016-04-09 11:11:18 -0500

    flight_data

--- api/controllers/FlightController.js
+++ api/controllers/FlightController.js
@@ -27,14 +27,37 @@ module.exports = {
       //flight by route, departing on given date
       //curl -v  -X GET "https://api.flightstats.com/flex/schedules/rest/v1/json/from/OMA/to/DFW/departing/2016/04/09?appId=0efb0de0&appKey=4cc49ed437eb48a4729722360e30ac41"
 
+      function process_response(webservice_response, trip, callback) {
+        var webservice_data = "";
+        webservice_response.on('error', function(e) {
+          console.log(e.message);
+          callback('Error: ' + e.message);
+        });
+        webservice_response.on('data', function(chunk) {
+          webservice_data += chunk;
+        });
+
+        webservice_response.on('end', function() {
+          flight_data = JSON.parse(webservice_data);
+          callback();
+        });
+      };
+
       function pick_flight(trip, callback) {
-       //Need to parse trip.departureDate for our API (/trip.year/trip.month/trip.day)
-       options = {
-       host: 'api.flightstats.com',
-       port: 80,
-       path: '/flex/flightstatus/rest/v2/json/route/status/'+trip.origin+'/'+trip.destination+'/dep/'+date_array[3]+'/'+date_array[2]+'/'+date_array[1],
-       method: 'GET'
-       };
+        options = {
+          host: 'api.flightstats.com',
+          port: 80,
+          path: '/flex/flightstatus/rest/v2/json/route/status/' + trip.origin + '/' + trip.destination + '/dep/' + date_array[3] + '/' + date_array[1] + '/' + date_array[2],
+          method: 'GET'
+        };
+
+        // This is asynchronous - won't guarantee we get the response before we use it
+        var webservice_request = http.request(options, function(response) {
+          process_response(response, trip, callback)
+        });
+        webservice_request.end();
+
+      };
 
     });
   },
