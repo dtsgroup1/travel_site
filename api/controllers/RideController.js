99531f00b31bb25951c0ae2a3769943ae6e1cd50
ClearlyJosh <jpclark@unomaha.edu>
2016-04-17 23:03:46 -0500

    Podracing
    -Controllers reformatted to allow sails to lift
    -Uber dependencies installed
    -Eradicated trip-dependent pages
    -Budget tiers eliminated (except in schema)
    -npm install async
    -Info now making it to search page.
    -Currently can't POST owner.id

--- api/controllers/RideController.js
+++ api/controllers/RideController.js
@@ -9,33 +9,15 @@
 // curl -H 'Authorization: Token 'kuhOpgaA1JkBbjFyJ7iaweUbCUVYZOrPlMFdMl8H' \'https://api.uber.com/v1/products?latitude=37.7759792&longitude=-122.41823'
 // npm install uber-api
 
-
-var uberLib = require('uber-api'),
-  token = 'kuhOpgaA1JkBbjFyJ7iaweUbCUVYZOrPlMFdMl8H',
-  Uber = new uberLib(token,'v1'),
-
-  //Lat and longitude will carry over from Hotel API
-  lat = 36,
-  lon = -94;
-
-Uber.getProducts(lat, lon, function(error, response) {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log(response);
-    console.log(body);
-  }
-});
-
 /** var Uber = require('uber-api')({server_token:'kuhOpgaA1JkBbjFyJ7iaweUbCUVYZOrPlMFdMl8H',version:'v1'}),
-  start_latitude = 41.3096180,
-  start_longitude = -96.1500320,
-  end_latitude = 41.2875140,
-  end_longitude = -96.1502320;
+ start_latitude = 41.3096180,
+ start_longitude = -96.1500320,
+ end_latitude = 41.2875140,
+ end_longitude = -96.1502320;
 
 
 
-Uber.getProducts(start_latitude, start_longitude,end_latitude, end_longitude, function(error, response) {
+ Uber.getProducts(start_latitude, start_longitude,end_latitude, end_longitude, function(error, response) {
   if (error) {
     console.log(error);
   } else {
@@ -43,14 +25,14 @@ Uber.getProducts(start_latitude, start_longitude,end_latitude, end_longitude, fu
   }
 });
 
-Uber.getPriceEstimate(start_latitude, start_longitude, end_latitude, end_longitude[callback]).then(function(response){
+ Uber.getPriceEstimate(start_latitude, start_longitude, end_latitude, end_longitude[callback]).then(function(response){
   console.log(response);
 }, function(error){
   console.error(response);
 });
 
 
-/** Uber.getProducts(lat, lon, function(error, response) {
+ /** Uber.getProducts(lat, lon, function(error, response) {
   if (error) {
     console.log(error);
   } else {
@@ -58,28 +40,48 @@ Uber.getPriceEstimate(start_latitude, start_longitude, end_latitude, end_longitu
   }
 });
 
-Uber.getProducts(lat, lon).then(function(response){
+ Uber.getProducts(lat, lon).then(function(response){
   console.log(response);
 }, function(error){
   console.error(response);
 });
+ */
 
 module.exports = {
 
-  'new': function(req, res) {
-    Trip.findOne(req.param('id')).populateAll().exec(function (err, trip) {
+  'new': function (req, res) {
+    Trip.findOne(req.param('owner'), function foundTrip(err, trip) {
       if (err) return next(err);
       if (!trip) return next();
-
-      //API will go here?
-      var http = require('http');
-      //trip.ride = null;
-
-
-
-      res.redirect('/trip/fun/' + trip.id);
+      res.view({
+        trip: trip
+      });
     });
   },
 
-}; */
+  'search': function (req, res, next) {
+    Trip.findOne(req.param('id')).populateAll().exec(function (err, trip) {
+      var uberLib = require('uber-api');
+      var http = require('http');
+
+        token = 'kuhOpgaA1JkBbjFyJ7iaweUbCUVYZOrPlMFdMl8H',
+        Uber = new uberLib(token, 'v1'),
+
+      //Lat and longitude will carry over from Hotel API
+        lat = 36,
+        lon = -94;
+
+      Uber.getProducts(lat, lon, function (error, response) {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log(response);
+          console.log(body);
+        }
+      });
+
+    });
+  }
+
+};
 
