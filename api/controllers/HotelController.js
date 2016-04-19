a85df7978cd7b46302d2de881e6ec92620b25510
brentmac94 <brent.mac94@yahoo.com>
2016-04-18 21:57:21 -0500

    Hotel API update

--- api/controllers/HotelController.js
+++ api/controllers/HotelController.js
@@ -5,6 +5,7 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 
+<<<<<<< Updated upstream
 module.exports = {
 
   'new': function (req, res) {
@@ -22,14 +23,34 @@ module.exports = {
     Trip.findOne(req.param('id')).populateAll().exec(function (err, trip) {
       if (err) return next(err);
       if (!trip) return next();
+=======
+
+
+/**
+
+
+ module.exports = {
+
+ 'new': function(req, res) {
+ Trip.findOne(req.param('id')).populateAll().exec(function (err, trip) {
+ if (err) return next(err);
+ if (!trip) return next(); */
+
+//API will go here?
+// var http = require('http');
+//trip.reservation = null;
+
+//http://api.hotwire.com/v1/search/hotel?apikey=abc123&dest=San%20Francisco,%20Ca.&rooms=1&adults=2&children=0&startdate=01/20/2014&enddate=01/23/2014
+>>>>>>> Stashed changes
 
       //http://api.hotwire.com/v1/search/hotel?apikey=qba8knkmf9ahgzbm6rmy988g&dest=San%20Francisco,%20Ca.&rooms=1&adults=2&children=0&startdate=04/20/2016&enddate=04/23/2016
       //var invalidKey = false;
       //appKey: qba8knkmf9ahgzbm6rmy988g
 
-      // Docs: http://app-framework-software.intel.com/api2/index.html#$_get
-      // API Help Source: https://github.com/gomobile/sample-masheryhotwire/blob/master/www/js/api.js
+// Docs: http://app-framework-software.intel.com/api2/index.html#$_get
+// API Help Source: https://github.com/gomobile/sample-masheryhotwire/blob/master/www/js/api.js
 
+<<<<<<< Updated upstream
       var Hotwire = require('hotwire');
       var http = require('http');
       trip.year = null;
@@ -104,5 +125,86 @@ module.exports = {
   }
 
 };
+=======
+// var apiKey = 'qba8knkmf9ahgzbm6rmy988g';
+// var invalidKey = false;
+
+/*****************************************/
+
+module.exports = {
+
+
+
+  show: function (req, res, next) {
+
+    var Hotwire = require('hotwire');
+
+//api key
+    var hotwire = new Hotwire('qba8knkmf9ahgzbm6rmy988g');
+    hotwire.hotelDeals({
+      format: 'json',
+      startdate: '04/28/2016',
+      dest: 'Omaha',
+      limit: '5'
+    }, function (err, response, body) {
+      if (err) {
+        console.log(err);
+      }
+      console.log(response.statusCode);
+      console.log(JSON.parse(body).Result);
+      var hotels = JSON.parse(body).Result;
+      
+
+      async.each([hotels], function (err) {
+        if (err) console.log(err);
+        //console.log('done');
+
+        res.view({
+          hotels: hotels
+        })
+
+      })
+
+    })
+
+  }
+
+}
+
+
+
+      /**
+
+       function process_response(webservice_response, hotel, callback) {
+       var webservice_data = "";
+       webservice_response.on('error', function (e) {
+       console.log(e.message);
+       callback("Error: " + e.message);
+       });
+       webservice_response.on('data', function (chunk) {
+       webservice_data += chunk;
+       });
+
+       webservice_response.on('end', function () {
+       hotel_data = JSON.parse(webservice_data);
+       hotel.price = hotel.Price;
+       var sumprice = 0;
+       var avgprice = 0;
+       sumPrice += hotel.Price;
+       avgPrice = (sumPrice / 5);
+
+
+       // console.log(stock.symbol + ' = $' + stock.current_price);
+       callback();
+       });
+
+
+        });
+    });
+    });
+  }
+}
+**/
+>>>>>>> Stashed changes
 
 
--- api/controllers/RideController.js
+++ api/controllers/RideController.js
@@ -9,6 +9,30 @@
 // curl -H 'Authorization: Token 'kuhOpgaA1JkBbjFyJ7iaweUbCUVYZOrPlMFdMl8H' \'https://api.uber.com/v1/products?latitude=37.7759792&longitude=-122.41823'
 // npm install uber-api
 
+<<<<<<< Updated upstream
+=======
+  /*
+
+var Uber = require('node-uber');
+
+var uberLib = require('uber-api'),
+  token = 'kuhOpgaA1JkBbjFyJ7iaweUbCUVYZOrPlMFdMl8H',
+  Uber = new uberLib(token,'v1'),
+
+  //Lat and longitude will carry over  to Hotel API
+  lat = 36,
+  lon = -94;
+
+Uber.getProducts(lat, lon, function(error, response) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(response);
+    console.log(body);
+  }
+});
+
+>>>>>>> Stashed changes
 /** var Uber = require('uber-api')({server_token:'kuhOpgaA1JkBbjFyJ7iaweUbCUVYZOrPlMFdMl8H',version:'v1'}),
  start_latitude = 41.3096180,
  start_longitude = -96.1500320,
